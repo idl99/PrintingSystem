e9c8ca38c48fce2cdc0fda4bf1cf62dc6749025d
Ihan Dilnath <ihandilnath2012@gmail.com>
2020-12-18 23:49:59 +0530

    multiple updates to LaserPrinter

--- src/main/java/uk/ac/westminster/ihanlelwala/printingsystem/LaserPrinter.java
+++ src/main/java/uk/ac/westminster/ihanlelwala/printingsystem/LaserPrinter.java
@@ -3,6 +3,7 @@ package uk.ac.westminster.ihanlelwala.printingsystem;
 /**
  * TODO add comments for the methods
  * https://www.geeksforgeeks.org/object-level-class-level-lock-java/
+ * https://www.baeldung.com/cs/monitor
  */
 public class LaserPrinter implements ServicePrinter {
 
@@ -11,21 +12,11 @@ public class LaserPrinter implements ServicePrinter {
     private int currentTonerLevel;
     private int numberOfDocumentsPrinted;
 
-    // Opting for eager initialization for better thread safety during initialization
-    // than lazy initialization (creating instance in the getInstance method)
-    private static LaserPrinter instance = new LaserPrinter();
-
-    // Although spec mentions to create a constructor, i have decided to make this a singleton class
-    // since its a best practice suitable in this scenario, where this PrintingSystem program is only for a single printer
-    private LaserPrinter() {
-    }
-
-    /**
-     *
-     * @return instance of Laser Printer
-     */
-    public static LaserPrinter getInstance() {
-        return instance;
+    public LaserPrinter(String id, int currentPaperLevel, int currentTonerLevel) {
+        this.id = id;
+        this.currentPaperLevel = currentPaperLevel;
+        this.currentTonerLevel = currentTonerLevel;
+        this.numberOfDocumentsPrinted = 0;
     }
 
     @Override
@@ -38,7 +29,7 @@ public class LaserPrinter implements ServicePrinter {
             System.out.println("Successfully replaced Toner Cartridge.");
         } else {
             System.out.printf("Cannot replace toner. Toner Level should be at a maximum of %d before it can be replaced." +
-                    "Current Toner Level is %d.", MINIMUM_TONER_LEVEL, currentTonerLevel);
+                    "Current Toner Level is %d.\n", MINIMUM_TONER_LEVEL, currentTonerLevel);
         }
     }
 
@@ -49,10 +40,10 @@ public class LaserPrinter implements ServicePrinter {
             System.out.println("Refilling printer with paper...");
             // Allow paper technician to refill paper
             currentPaperLevel += SHEETS_PER_PACK;
-            System.out.printf("Refilled tray with pack of paper. New Paper Level: %d", currentPaperLevel);
+            System.out.printf("Refilled tray with pack of paper. New Paper Level: %d\n", currentPaperLevel);
         } else {
             System.out.printf("Cannot refill printer with paper at this time. Current paper level is %d. " +
-                    "Maximum paper level is %d", currentPaperLevel, FULL_PAPER_TRAY);
+                    "Maximum paper level is %d.\n", currentPaperLevel, FULL_PAPER_TRAY);
         }
     }
 
@@ -63,20 +54,24 @@ public class LaserPrinter implements ServicePrinter {
         boolean sufficientTonerLevel = numberOfPages <= currentTonerLevel;
 
         if(sufficientPaperLevel && sufficientTonerLevel) {
-            System.out.println("Printing document...");
+            System.out.printf("Printing document with page length: %d.\n", numberOfPages);
             currentPaperLevel -= numberOfPages;
             currentTonerLevel -= numberOfPages;
             numberOfDocumentsPrinted++;
-            System.out.printf("Successfully printed the document. New Paper Level is %d and Toner Level is %d.",
+            System.out.printf("Successfully printed the document. New Paper Level is %d and Toner Level is %d.\n",
                     currentPaperLevel, currentTonerLevel);
         } else {
             // User cannot print
-            if(!sufficientPaperLevel) {
-                System.out.printf("Out of paper. Current Paper Level is %d. The document you wish to print is %d pages.",
+            if(!sufficientPaperLevel && !sufficientTonerLevel) {
+                System.out.printf("Out of paper and toner. Current Paper Level is %d and Toner Level is %d. The document you wish to print is %d pages.\n",
+                        currentPaperLevel, currentTonerLevel, numberOfPages);
+            }
+            else if(!sufficientPaperLevel) {
+                System.out.printf("Out of paper. Current Paper Level is %d. The document you wish to print is %d pages.\n",
                         currentPaperLevel, numberOfPages);
             }
-            if(!sufficientTonerLevel) {
-                System.out.printf("Out of toner. Current Toner Level is %d. The document you wish to print is %d pages.",
+            else {
+                System.out.printf("Out of toner. Current Toner Level is %d. The document you wish to print is %d pages.\n",
                         currentTonerLevel, numberOfPages);
             }
         }
